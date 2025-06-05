358f02df59de4f243b84778d38d27522245b293e
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2025-06-05 12:38:46 +0530

    feat: add solution for removeDuplicates 2

--- array/remove-duplicates-from-sorted-arrays.ts
+++ array/remove-duplicates-from-sorted-arrays.ts
@@ -76,7 +76,3 @@ const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 const k = removeDuplicates(nums);
 console.log("nums: ", nums);
 console.log("k: ", k);
-
-console.log(
-  "Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively: "
-);
